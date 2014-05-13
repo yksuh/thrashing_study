13be3dd0d36ed65114fb998e18879a4af811dc6f
Young-K. Suh <yksuh@dhcp-10-134-219-89.uawifi.arizona.edu>
2014-05-12 19:03:39 -0700

    Adds copy script

---
+++ copy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+scp src/azdblab/plugins/scenario/ScenarioBasedOnTransaction.java yksuh@lectura.cs.arizona.edu:~/GitHub/thrashing_study/src/azdblab/plugins/scenario/
+scp plugins/plugins/XactThrashingScenario.java yksuh@lectura.cs.arizona.edu:~/GitHub/thrashing_study/plugins/plugins/

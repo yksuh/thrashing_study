a6c4a966bbb7895f1aa5354390608e5353d17c87
Youngkyoon Suh <yksuh@on-campus-115-104.vpn.arizona.edu>
2014-08-24 19:29:32 +0900

    Revert 79bfd05..7ab23a4
    This rolls back to commit 79bfd056690847254e10bdc63fc69bcaac647db9.

--- ant_terarun.sh
+++ ant_terarun.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-
-#!/bin/sh
 git pull origin TeradataCare
 ant -Dexec_dir=/cs/projects/tau/installations/azdblab/thrashing_study -Dplugins.dir=/cs/projects/tau/installations/azdblab/thrashing_study/plugins AZDBLAB xtplugin xtsch

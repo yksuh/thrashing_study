79bfd056690847254e10bdc63fc69bcaac647db9
Youngkyoon Suh <yksuh@on-campus-115-104.vpn.arizona.edu>
2014-08-24 19:11:05 +0900

    build script for thrashing code on teradata

---
+++ ant_terarun.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+git pull origin TeradataCare
+ant -Dexec_dir=/cs/projects/tau/installations/azdblab/thrashing_study -Dplugins.dir=/cs/projects/tau/installations/azdblab/thrashing_study/plugins AZDBLAB xtplugin xtsch

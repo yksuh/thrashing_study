eb65cc477af74d2ac067e230304420f1c0bf82f8
Youngkyoon Suh <yksuh@yksuh.local>
2014-08-29 14:28:38 +0900

    Ant script modified

--- ant_run.sh
+++ ant_run.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+git checkout origin/TeradataCare
+git pull origin TeradataCare
 ant -Dexec_dir=/cs/projects/tau/installations/azdblab/thrashing_study -Dplugins.dir=/cs/projects/tau/installations/azdblab/thrashing_study/plugins AZDBLAB xtplugin xtsch
--- ant_terarun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-git pull origin TeradataCare
-ant -Dexec_dir=/cs/projects/tau/installations/azdblab/thrashing_study -Dplugins.dir=/cs/projects/tau/installations/azdblab/thrashing_study/plugins AZDBLAB xtplugin xtsch

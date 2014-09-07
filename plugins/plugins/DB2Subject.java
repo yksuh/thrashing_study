59a266aed1f9d4fef3cbc4fd431a48d2dc19fe0e
Young-K. Suh <yksuh@yksuh-mbpro.local>
2014-09-08 01:47:24 +0900

    DB2 batch update

--- plugins/plugins/DB2Subject.java
+++ plugins/plugins/DB2Subject.java
@@ -128,14 +128,14 @@ public class DB2Subject extends ExperimentSubject {
 		          }
 		          strupdate = "INSERT INTO " + tableName + " VALUES(" + i + "," +
 		                      strdata + ")";
-		          _statement.executeUpdate(strupdate);
-//		          _statement.addBatch(strupdate);
-//		          if((i + 1) % 30000 == 0){
-//		        	  _statement.executeBatch();
-//		        	  commit();
-//		          }
+//		          _statement.executeUpdate(strupdate);
+		          _statement.addBatch(strupdate);
+		          if((i + 1) % 30000 == 0){
+		        	  _statement.executeBatch();
+		        	  commit();
+		          }
 		        }
-//		      _statement.executeBatch();
+		      _statement.executeBatch();
 		      commit();
 		    } catch (SQLException sqlex){
 		      sqlex.printStackTrace();

11df9edc65f953b0c3682a54a99d77c0da4b099c
Youngkyoon Suh <yksuh@yksuh.local>
2014-09-12 17:39:52 +0900

    update transaction investigation

--- ant_run.sh
+++ ant_run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-git pull origin master
-ant -Dexec_dir=/cs/projects/tau/installations/azdblab/thrashing_study -Dplugins.dir=/cs/projects/tau/installations/azdblab/thrashing_study/plugins AZDBLAB xtplugin xtsch
-cp /cs/projects/tau/installations/azdblab/thrashing_study/azdblab.jar ~/sqlserver/
+git pull origin BatchsetGenerator
+ant -Dexec_dir=/cs/projects/tau/installations/azdblab/thrashing_study -Dplugins.dir=/cs/projects/tau/installations/azdblab/thrashing_study/plugins AZDBLAB xtplugin xtsch 
 cp /cs/projects/tau/installations/azdblab/thrashing_study/plugins/Xact*.jar ~/sqlserver/
+cp /cs/projects/tau/installations/azdblab/thrashing_study/azdblab.jar ~/sqlserver/
+
